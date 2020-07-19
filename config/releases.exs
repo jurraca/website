d64ef31690a18a576c163e8f40a414aba7cd2758
ju <julienurraca@gmail.com>
2020-07-19 18:14:48 -0500

    update releases config

--- config/releases.exs
+++ config/releases.exs
@@ -9,6 +9,9 @@ import Config
 # kept out of version control and might be hard to recover
 # or recreate for your teammates (or yourself later on).
 config :website, WebsiteWeb.Endpoint,
+  http: [:inet6, port: System.get_env("PORT") || 4000],
+  url: [scheme: "https", host: "julienurraca.com", port: 443],
+  cache_static_manifest: "priv/static/cache_manifest.json"
   secret_key_base: System.get_env("SECRET_KEY_BASE")
 
 # Configure your database
